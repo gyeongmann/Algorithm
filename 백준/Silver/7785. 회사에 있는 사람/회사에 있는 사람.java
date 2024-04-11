d4f01f2e64f7bfe3ad9d9830eab171643c4d97b1
gyeongmann <128693089+gyeongmann@users.noreply.github.com>
2024-04-11 10:51:17 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 1564 ms, Memory: 61316 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -1,41 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.StringTokenizer;
+import java.util.TreeSet;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		Map<String, Boolean> map = new HashMap<>();
+		TreeSet<String> set = new TreeSet<>(new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				// TODO Auto-generated method stub
+				return o2.compareTo(o1);
+			}
+		});
 		int n = Integer.parseInt(in.readLine());
 		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(in.readLine());
 			String name = st.nextToken();
 			String status = st.nextToken();
 			if (status.equals("enter")) {
-				map.put(name, true);
+				set.add(name);
 			} else {
-				map.remove(name);
+				set.remove(name);
 			}
 		}
 		
-		List<String> names = new ArrayList<>();
-		for (String name : map.keySet()) {
-			names.add(name);
-		}
-		names.sort(new Comparator<String>() {
-			@Override
-			public int compare(String o1, String o2) {
-				return o2.compareTo(o1);
-			}
-		});
-		for (String name : names) {
+		for (String name : set) {
 			System.out.println(name);
 		}
 	}
