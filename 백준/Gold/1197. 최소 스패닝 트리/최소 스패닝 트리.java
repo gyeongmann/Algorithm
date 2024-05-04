eacb943155e3be2b9e48d704a720308bd3f20230
gyeongmann <128693089+gyeongmann@users.noreply.github.com>
2024-05-04 15:30:26 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 572 ms, Memory: 56724 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static class Edge implements Comparable<Edge> {
+		int to, weight;
+
+		public Edge(int to, int weight) {
+			super();
+			this.to = to;
+			this.weight = weight;
+		}
+
+		@Override
+		public int compareTo(Edge o) {
+			return Integer.compare(this.weight, o.weight);
+		}
+
+		@Override
+		public String toString() {
+			return "Edge [to=" + to + ", weight=" + weight + "]";
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+
+		int V = Integer.parseInt(st.nextToken());
+		int E = Integer.parseInt(st.nextToken());
+
+		List<Edge>[] adj = new ArrayList[V + 1];
+		for (int i = 1; i <= V; i++) {
+			adj[i] = new ArrayList<>();
+		}
+		
+		for (int i = 0; i < E; i++) {
+			st = new StringTokenizer(in.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int weight = Integer.parseInt(st.nextToken());
+			
+			adj[a].add(new Edge(b, weight));
+			adj[b].add(new Edge(a, weight));
+		} // input
+		
+//		for (int i = 1; i <= V; i++) {
+//			System.out.println(adj[i]);
+//		}
+		
+		boolean[] visited = new boolean[V + 1];
+		PriorityQueue<Edge> pq = new PriorityQueue<>();
+		visited[1] = true;
+		for (Edge e : adj[1]) {
+			pq.offer(e);
+		}
+		
+		long ans = 0;
+		int cnt = 0;
+		while (cnt < V - 1) {
+			Edge curr = pq.poll();
+			int to = curr.to;
+			if (visited[to]) continue;
+			visited[to] = true;
+			cnt++;
+			ans += curr.weight;
+			for (Edge e : adj[to]) {
+				if (!visited[e.to]) {
+					pq.offer(e);
+				}
+			}
+		}
+		
+		System.out.println(ans);
+	}
+}
