71417ad92f0a4cb2a0ce68e1e7a24c46369a30bf
gyeongmann <128693089+gyeongmann@users.noreply.github.com>
2024-07-06 13:31:02 +0900

    [Bronze V] Title: 이상한 기호, Time: 116 ms, Memory: 12884 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        long ans = (A-B) * (A+B);
+        System.out.println(ans);
+    }
+}
