ffcdb33e5f00c3715b388c772a457f81014abf75
gyeongmann <hkc8857@naver.com>
2024-11-05 00:44:21 +0900

    [Gold II] Title: 대표 선수, Time: 1704 ms, Memory: 160584 KB -BaekjoonHub

--- 백준/Gold/2461. 대표 선수/대표 선수.java
+++ 백준/Gold/2461. 대표 선수/대표 선수.java
@@ -1,60 +1,73 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	static int N, M;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()); // 학급 수
+        int M = Integer.parseInt(st.nextToken()); // 학생 수
 
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+        // 학급, 능력치
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return Integer.compare(o1[1], o2[1]);
+            }
+        });
 
-		int[][] arr = new int[N][M];
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < M; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
+        for (int classNumber = 0; classNumber < N; classNumber++) {
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < M; i++) {
+                pq.offer(new int[] {classNumber, Integer.parseInt(st.nextToken())});
+            }
+        }
 
-		for (int i = 0; i < N; i++) {
-			Arrays.sort(arr[i]);
-		}
+//        while (!pq.isEmpty()) {
+//            System.out.println(Arrays.toString(pq.poll()));
+//        }
 
-		int[] idx = new int[N];
+        int answer = Integer.MAX_VALUE;
+        int[] arr = new int[N];
+        Arrays.fill(arr, -1);
+        boolean keepLoop = true;
+        while (keepLoop) {
+            keepLoop = false;
+            int[] curr = pq.poll();
+            arr[curr[0]] = curr[1];
 
-		int ans = 1_000_000_001;
-		for (int j = 0; j < N * M; j++) {
-			int min = arr[0][idx[0]];
-			int max = arr[0][idx[0]];
-			int minIdx = 0;
-			int maxIdx = 0;
-			for (int i = 1; i < N; i++) {
-				if (arr[minIdx][idx[minIdx]] > arr[i][idx[i]]) {
-					minIdx = i;
-					min = arr[minIdx][idx[minIdx]];
-				}
+            for (int i = 0; i < N; i++) {
+                if (arr[i] == -1) keepLoop = true;
+            }
+//            System.out.println(Arrays.toString(arr));
+        }
 
-				if (arr[maxIdx][idx[maxIdx]] < arr[i][idx[i]]) {
-					maxIdx = i;
-					max = arr[maxIdx][idx[maxIdx]];
-				}
-			}
+//        System.out.println("-------------------------");
+        int min = Integer.MAX_VALUE;
+        int max = -1;
+        for (int i = 0; i < N; i++) {
+            min = Math.min(min, arr[i]);
+            max = Math.max(max, arr[i]);
+        }
+        answer = Math.min(answer, max - min);
 
-			ans = Math.min(ans, max - min);
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            arr[curr[0]] = curr[1];
 
-			idx[minIdx]++;
-			if (idx[minIdx] == M) {
-				break;
-			}
-		}
-
-		System.out.println(ans);
-	}
+//            System.out.println(Arrays.toString(arr));
+            min = Integer.MAX_VALUE;
+            max = -1;
+            for (int i = 0; i < N; i++) {
+                min = Math.min(min, arr[i]);
+                max = Math.max(max, arr[i]);
+            }
+            answer = Math.min(answer, max - min);
+        }
+        System.out.println(answer);
+    }
 }
