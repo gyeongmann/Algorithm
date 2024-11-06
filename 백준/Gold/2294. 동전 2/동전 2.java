1404f73a51a5f63200fda70a96843993077b909e
gyeongmann <hkc8857@naver.com>
2024-11-06 22:54:45 +0900

    [Gold V] Title: 동전 2, Time: 84 ms, Memory: 11840 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -1,39 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static final int INF = 987654321;
-	public static void main(String[] args) throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(in.readLine()); // n, k
-		int n = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(st.nextToken());
-
-		int[] coins = new int[n + 1];
-		for (int i = 1; i <= n; i++) {
-			coins[i] = Integer.parseInt(in.readLine());
-		}
-
-		int[] dp = new int[k + 1];
-		for (int i = 1; i <= k; i++) { // 초기값
-			dp[i] = INF;
-		}
-
-		for (int i = 1; i <= n; i++) {
-			int coin = coins[i];
-			for (int j = 1; j <= k; j++) {
-				if (j >= coin) {
-					dp[j] = Math.min(dp[j], dp[j - coin] + 1);
-				}
-			}
-		}
-
-		if (dp[k] == INF) {
-			System.out.println(-1);
-			return;
-		}
-		System.out.println(dp[k]);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] dp = new int[k+1];
+        for (int i = 1; i <= k; i++) {
+            dp[i] = 987654321;
+        }
+        for (int i = 0; i < n; i++) {
+            int coin = Integer.parseInt(br.readLine());
+            for (int j = coin; j <= k; j++) {
+                dp[j] = Math.min(dp[j], dp[j-coin]+1);
+            }
+        }
+        
+        if (dp[k] == 987654321) {
+            System.out.println(-1);
+            return;
+        }
+        System.out.println(dp[k]);
+    }
 }
