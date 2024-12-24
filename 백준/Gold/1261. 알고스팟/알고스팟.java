694dbd6fad166c5dc1c7150d0cf9c4b169325d24
gyeongmann <hkc8857@naver.com>
2024-12-24 16:07:10 +0900

    [Gold IV] Title: 알고스팟, Time: 76 ms, Memory: 12152 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -1,89 +1,57 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static class Point implements Comparable<Point> {
-		int r, c, weight;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-		public Point(int r, int c, int weight) {
-			super();
-			this.r = r;
-			this.c = c;
-			this.weight = weight;
-		}
+        st = new StringTokenizer(br.readLine());
 
-		@Override
-		public int compareTo(Point o) {
-			return Integer.compare(this.weight, o.weight);
-		}
-	}
+        int C = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
 
-	static int N, M; // 열, 행
-	static int[][] map, dist;
-	static boolean[][] vis;
-	static final int INF = 10_000;
+        int[][] map = new int[R][C];
+        for(int i = 0; i < R; i++) {
+            String row = br.readLine();
+            for (int j = 0; j < C; j++) {
+                map[i][j] = row.charAt(j) - '0';
+            }
+        }
 
-	static int[] dr = { 1, 0, -1, 0 };
-	static int[] dc = { 0, 1, 0, -1 };
+        Deque<int[]> deque = new ArrayDeque<>();
+        deque.offer(new int[] {0, 0, map[0][0]});
+        boolean[][] vis = new boolean[R][C];
+        vis[0][0] = true;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(in.readLine()); // N, M
+        int[] dr = {1, 0, -1, 0};
+        int[] dc = {0, 1, 0, -1};
 
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+        while(!deque.isEmpty()) {
+            int[] curr = deque.pollFirst();
+            int r = curr[0];
+            int c = curr[1];
+            int sum = curr[2];
 
-		map = new int[M][N];
+            if (r == R-1 && c == C-1) {
+                System.out.println(sum);
+                return;
+            }
 
-		for (int r = 0; r < M; r++) {
-			String line = in.readLine();
-			for (int c = 0; c < N; c++) {
-				map[r][c] = Character.getNumericValue(line.charAt(c));
-			}
-		}
+            for(int i = 0; i < 4; i++) {
+                int nr = r + dr[i];
+                int nc = c + dc[i];
 
-		Dijkstra(0, 0);
-		System.out.println(dist[M - 1][N - 1]);
-	}
+                if(nr < 0 || nc < 0 || nr >= R || nc >= C) continue;
+                if(vis[nr][nc]) continue;
 
-	private static void Dijkstra(int r, int c) {
-		PriorityQueue<Point> pq = new PriorityQueue<>();
-		vis = new boolean[M][N];
-		dist = new int[M][N];
-		for (int[] row : dist) {
-			Arrays.fill(row, INF);
-		}
-
-		pq.add(new Point(r, c, 0));
-		dist[0][0] = map[r][c];
-
-		while (!pq.isEmpty()) {
-			Point curr = pq.poll();
-
-			int row = curr.r;
-			int col = curr.c;
-			if (vis[row][col])
-				continue;
-			vis[row][col] = true;
-
-			for (int dir = 0; dir < 4; dir++) {
-				int nr = row + dr[dir];
-				int nc = col + dc[dir];
-				if (isValid(nr, nc) && !vis[nr][nc] && dist[nr][nc] > map[nr][nc] + dist[row][col]) {
-					dist[nr][nc] = map[nr][nc] + dist[row][col];
-					pq.add(new Point(nr, nc, dist[nr][nc]));
-				}
-			}
-		}
-	}
-
-	private static boolean isValid(int nr, int nc) {
-		if (nr < 0 || nc < 0 || nr >= M || nc >= N)
-			return false;
-		return true;
-	}
+                if(map[nr][nc] == 0) {
+                    deque.offerFirst(new int[] {nr, nc, sum});
+                } else {
+                    deque.offerLast(new int[] {nr, nc, sum+1});
+                }
+                vis[nr][nc] = true;
+            }
+        }
+    }
 }
