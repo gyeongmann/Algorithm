79e3c86cc4e90444c69ff45ff839bbd4952823a8
gyeongmann <hkc8857@naver.com>
2024-12-26 14:26:15 +0900

    [Silver I] Title: 호반우 상인의 이상한 품질 계산법, Time: 228 ms, Memory: 22928 KB -BaekjoonHub

--- 백준/Silver/20117. 호반우 상인의 이상한 품질 계산법/호반우 상인의 이상한 품질 계산법.java
+++ 백준/Silver/20117. 호반우 상인의 이상한 품질 계산법/호반우 상인의 이상한 품질 계산법.java
@@ -8,27 +8,31 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        Deque<Integer> deque = new ArrayDeque<>();
+
         int[] arr = new int[N];
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
-
-        for (int i = 0; i < N; i++) {
-            deque.offer(arr[i]);
+        
+        if(N == 1) {
+            System.out.println(arr[0]);
+            return;
         }
-
+        
         int answer = 0;
-        while(deque.size() >= 2) {
-            int first = deque.pollFirst();
-            int last = deque.pollLast();
 
-            answer += last * 2;
+        int first = 0;
+        int last = N - 1;
+
+        while(first < last) {
+            answer += arr[last] * 2;
+            first++;
+            last--;
         }
 
-        if (!deque.isEmpty()) {
-            answer += deque.pollFirst();
+        if(first == last) {
+            answer += arr[first];
         }
 
         System.out.println(answer);
