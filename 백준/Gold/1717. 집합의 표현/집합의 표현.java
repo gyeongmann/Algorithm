b71d6456eda4245ab7889eef6efcb185a78458c7
gyeongmann <hkc8857@naver.com>
2025-01-01 16:33:42 +0900

    [Gold V] Title: 집합의 표현, Time: 324 ms, Memory: 48596 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int[] p;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        init(n);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int op = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (op == 0) {
+                union(a, b);
+                continue;
+            }
+
+            if (find(a) == find(b)) {
+                sb.append("YES").append('\n');
+            } else {
+                sb.append("NO").append('\n');
+            }
+        }
+
+        System.out.print(sb);
+    }
+
+    static void init(int n) {
+        p = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            p[i] = i;
+        }
+    }
+
+    static void union(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
+        p[pb] = pa;
+    }
+
+    static int find(int x) {
+        if (p[x] == x)
+            return x;
+        return p[x] = find(p[x]);
+    }
+}
