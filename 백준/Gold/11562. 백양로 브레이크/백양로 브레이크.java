6f3a8d9884618f5ac809db9f253d53b47b66d5f1
gyeongmann <hkc8857@naver.com>
2025-01-02 15:33:41 +0900

    [Gold III] Title: 백양로 브레이크, Time: 292 ms, Memory: 29668 KB -BaekjoonHub

---
+++ 백준/Gold/11562. 백양로 브레이크/백양로 브레이크.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[][] dist;
+    static int INF = 300;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        dist = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j) {
+                    dist[i][j] = 0;
+                    continue;
+                }
+                dist[i][j] = INF;
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            dist[u][v] = 0;
+            if (b == 1) {
+                dist[v][u] = 0;
+            } else {
+                dist[v][u] = 1;
+            }
+        }
+
+        // floyd
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+
+        int ask = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int q = 0; q < ask; q++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            sb.append(dist[s][e]).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
