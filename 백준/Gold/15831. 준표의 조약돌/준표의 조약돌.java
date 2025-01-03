9bcd18ac7dacb1fc9b0ad4b1787e8cff972d94b8
gyeongmann <hkc8857@naver.com>
2025-01-03 17:44:16 +0900

    [Gold IV] Title: 준표의 조약돌, Time: 204 ms, Memory: 28652 KB, Score: 250 point -BaekjoonHub

--- 백준/Gold/15831. 준표의 조약돌/준표의 조약돌.java
+++ 백준/Gold/15831. 준표의 조약돌/준표의 조약돌.java
@@ -10,47 +10,44 @@ public class Main {
         int W = Integer.parseInt(st.nextToken());
 
         String s = br.readLine();
-
-        if (N == 1) {
-            if (s.equals("B") && B >= 1) {
-                System.out.println(1);
-                return;
-            } else if (s.equals("W") && B == 0) {
-                System.out.println(1);
-                return;
-            }
-            System.out.println(0);
-            return;
+        char[] arr = new char[N + 1];
+        for (int i = 1; i <= N; i++) {
+            arr[i] = s.charAt(i - 1);
         }
+        int[][] prefix = new int[2][N + 1];
 
-        char[] arr = s.toCharArray();
+        List<Integer> beforeBlack = new ArrayList<>();
+        for (int i = 1; i <= N; i++) {
+            if (arr[i] == 'B') {
+                prefix[0][i] = prefix[0][i - 1] + 1;
+                prefix[1][i] = prefix[1][i - 1];
+                beforeBlack.add(i - 1);
+            } else {
+                prefix[0][i] = prefix[0][i - 1];
+                prefix[1][i] = prefix[1][i - 1] + 1;
+            }
+        }
+        beforeBlack.add(N);
 
         int answer = 0;
-        for (int i = 0; i < N; i++) { // 시작점
-            int[][] dp = new int[2][N];
-            if (arr[i] == 'B') {
-                dp[0][i] = 1;
-            } else {
-                dp[1][i] = 1;
-            }
-            for (int j = i + 1; j < N; j++) { // 현재 위치
-                if (arr[j] == 'B') {
-                    dp[0][j] = dp[0][j - 1] + 1;
-                    dp[1][j] = dp[1][j - 1];
-                    if (dp[0][j] > B)
-                        break;
-                } else {
-                    dp[0][j] = dp[0][j - 1];
-                    dp[1][j] = dp[1][j - 1] + 1;
-                }
+        int left = 0;
+        int right = 0;
+        int idx = 0;
+        int size = beforeBlack.size();
 
-                if (dp[0][j] <= B && dp[1][j] >= W) {
-                    answer = Math.max(answer, j - i + 1);
+        while (idx < size) {
+            right = beforeBlack.get(idx);
+
+            if (prefix[0][right] - prefix[0][left] <= B) {
+                if (prefix[1][right] - prefix[1][left] >= W) {
+                    answer = Math.max(answer, right - left);
                 }
+                idx++;
+            } else {
+                left++;
             }
         }
 
         System.out.println(answer);
     }
-
 }
