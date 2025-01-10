b8628ab57dc07230b37eefd43da089aad54afee3
gyeongmann <hkc8857@naver.com>
2025-01-10 12:05:27 +0900

    [Gold III] Title: 같이 눈사람 만들래?, Time: 152 ms, Memory: 12036 KB -BaekjoonHub

--- 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.java
+++ 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.java
@@ -2,54 +2,39 @@ import java.io.*;
 import java.util.*;;
 
 public class Main {
-    static class Pair implements Comparable<Pair> {
-        long sum;
-        int idx1, idx2;
-
-        Pair(int x, int y, int idx1, int idx2) {
-            this.sum = x + y;
-            this.idx1 = idx1;
-            this.idx2 = idx2;
-        }
-
-        public int compareTo(Pair o) {
-            return Long.compare(this.sum, o.sum);
-        }
-
-        public boolean isOverlap(Pair o) {
-            if (this.idx1 == o.idx1 || this.idx2 == o.idx1 || this.idx1 == o.idx2 || this.idx2 == o.idx2) {
-                return true;
-            }
-            return false;
-        }
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int[] arr = new int[N];
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 모든 Pair 계산 후 리스트에 저장
-        List<Pair> pairs = new ArrayList<>();
+        Arrays.sort(arr);
+
+        int ans = Integer.MAX_VALUE;
         for (int i = 0; i < N; i++) {
-            for (int j = i + 1; j < N; j++) {
-                pairs.add(new Pair(arr[i], arr[j], i, j));
-            }
-        }
+            for (int j = i + 3; j < N; j++) {
+                int left = i + 1;
+                int right = j - 1;
+                while (left < right) {
+                    int sum = arr[i] + arr[j];
+                    int curr = arr[left] + arr[right];
 
-        // Pair 리스트 정렬
-        Collections.sort(pairs);
-
-        long ans = Long.MAX_VALUE;
-        for (int i = 0; i < pairs.size() - 1; i++) {
-            Pair curr = pairs.get(i);
-            Pair next = pairs.get(i + 1);
-            if (!curr.isOverlap(next)) {
-                ans = Math.min(ans, Math.abs(next.sum - curr.sum));
+                    ans = Math.min(ans, Math.abs(sum - curr));
+                    if (sum > curr) {
+                        left++;
+                    } else if (sum < curr) {
+                        right--;
+                    } else {
+                        System.out.println(0);
+                        return;
+                    }
+                }
             }
         }
 
