6bf966031c57422eaa8b4412a08fa7e65306235f
gyeongmann <hkc8857@naver.com>
2025-01-22 19:00:17 +0900

    [Gold V] Title: 벼락치기, Time: 92 ms, Memory: 12688 KB -BaekjoonHub

--- 백준/Gold/14728. 벼락치기/벼락치기.java
+++ 백준/Gold/14728. 벼락치기/벼락치기.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -13,34 +12,17 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int T = Integer.parseInt(st.nextToken());
 
-        int[][] arr = new int[N + 1][2];
-        for (int i = 1; i <= N; i++) {
+        int[] dp = new int[T + 1];
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int K = Integer.parseInt(st.nextToken());
             int S = Integer.parseInt(st.nextToken());
-            arr[i][0] = K;
-            arr[i][1] = S;
-        }
 
-        int[][] dp = new int[N + 1][T + 1];
-        for (int i = 1; i <= N; i++) {
-            int currK = arr[i][0];
-            int currS = arr[i][1];
-
-            if (currK > T) {
-                for (int t = 0; t <= T; t++) {
-                    dp[i][t] = dp[i - 1][t];
-                }
-                continue;
-            }
-            for (int t = 0; t < currK; t++) {
-                dp[i][t] = dp[i - 1][t];
-            }
-            for (int t = currK; t <= T; t++) {
-                dp[i][t] = Math.max(dp[i - 1][t], dp[i - 1][t - currK] + currS);
+            for (int t = T; t >= K; t--) {
+                dp[t] = Math.max(dp[t], dp[t - K] + S);
             }
         }
 
-        System.out.println(dp[N][T]);
+        System.out.println(dp[T]);
     }
 }
