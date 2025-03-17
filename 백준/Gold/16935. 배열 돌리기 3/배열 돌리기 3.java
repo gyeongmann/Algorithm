d9b093ca839684da7bee1c41512c35cebb9cb016
gyeongmann <hkc8857@naver.com>
2025-03-17 10:59:07 +0900

    [Gold V] Title: 배열 돌리기 3, Time: 272 ms, Memory: 44768 KB -BaekjoonHub

--- 백준/Gold/16935. 배열 돌리기 3/배열 돌리기 3.java
+++ 백준/Gold/16935. 배열 돌리기 3/배열 돌리기 3.java
@@ -3,141 +3,135 @@ import java.io.*;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+    static int N, M, R;
+    static int[][] map;
 
-		st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int R = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
 
-		int[][] map = new int[N][M];
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < M; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
+        map = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
-		st = new StringTokenizer(br.readLine());
-		for (int idx = 0; idx < R; idx++) {
-			int op = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        while (R-- > 0) {
+            int op = Integer.parseInt(st.nextToken());
+            processOperation(op);
+        }
 
-			if (op == 1) {
-				int r = map.length;
-				int c = map[0].length;
-				int[][] nmap = new int[r][c];
+        printMap();
+    }
 
-				for (int i = 0; i < r; i++) {
-					nmap[i] = map[r-i-1];
-				}
-				map = nmap;
-			} else if (op == 2) {
-				int r = map.length;
-				int c = map[0].length;
-				int[][] nmap = new int[r][c];
+    static void processOperation(int op) {
+        if (op == 1) flipVertical();
+        else if (op == 2) flipHorizontal();
+        else if (op == 3) rotateRight();
+        else if (op == 4) rotateLeft();
+        else if (op == 5) moveClockwise();
+        else if (op == 6) moveCounterClockwise();
+    }
 
-				for (int i = 0; i < r; i++) {
-					for (int j = 0; j < c; j++) {
-						nmap[i][j] = map[i][c-1-j];
-					}
-				}
-				map = nmap;
-			} else if (op == 3) {
-				int r = map.length;
-				int c = map[0].length;
-				int[][] nmap = new int[c][r];
+    static void flipVertical() {
+        for (int i = 0; i < N / 2; i++) {
+            int[] temp = map[i];
+            map[i] = map[N - 1 - i];
+            map[N - 1 - i] = temp;
+        }
+    }
 
-				for (int i = 0; i < c; i++) {
-					for (int j = 0; j < r; j++) {
-						nmap[i][j] = map[r-1-j][i];
-					}
-				}
-				map = nmap;
-			} else if (op == 4) {
-				int r = map.length;
-				int c = map[0].length;
-				int[][] nmap = new int[c][r];
+    static void flipHorizontal() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0, k = M - 1; j < k; j++, k--) {
+                int temp = map[i][j];
+                map[i][j] = map[i][k];
+                map[i][k] = temp;
+            }
+        }
+    }
 
-				for (int i = 0; i < c; i++) {
-					for (int j = 0; j < r; j++) {
-						nmap[i][j] = map[j][c-1-i];
-					}
-				}
-				map = nmap;
-			} else if (op == 5) {
-			    int r = map.length;
-				int c = map[0].length;
-				int[][] nmap = new int[r][c];
-				
-				for (int i = 0; i < r/2; i++) {
-				    for (int j = 0; j < c/2; j++) {
-				        nmap[i][j] = map[i+r/2][j];
-				    }
-				}
-				
-				for (int i = 0; i < r/2; i++) {
-				    for (int j = c/2; j < c; j++) {
-				        nmap[i][j] = map[i][j-c/2];
-				    }
-				}
-				
-				for (int i = r/2; i < r; i++) {
-				    for (int j = c/2; j < c; j++) {
-				        nmap[i][j] = map[i-r/2][j];
-				    }
-				}
-				
-				for (int i = r/2; i < r; i++) {
-				    for (int j = 0; j < c/2; j++) {
-				        nmap[i][j] = map[i][j+c/2];
-				    }
-				}
-				
-				map = nmap;
-			} else if (op == 6) {
-			    int r = map.length;
-				int c = map[0].length;
-				int[][] nmap = new int[r][c];
-				
-				for (int i = 0; i < r/2; i++) {
-				    for (int j = 0; j < c/2; j++) {
-				        nmap[i][j] = map[i][j+c/2];
-				    }
-				}
-				
-				for (int i = 0; i < r/2; i++) {
-				    for (int j = c/2; j < c; j++) {
-				        nmap[i][j] = map[i+r/2][j];
-				    }
-				}
-				
-				for (int i = r/2; i < r; i++) {
-				    for (int j = c/2; j < c; j++) {
-				        nmap[i][j] = map[i][j-c/2];
-				    }
-				}
-				
-				for (int i = r/2; i < r; i++) {
-				    for (int j = 0; j < c/2; j++) {
-				        nmap[i][j] = map[i-r/2][j];
-				    }
-				}
-				
-				map = nmap;
-			}
-		}
+    static void rotateRight() {
+        int[][] newMap = new int[M][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                newMap[j][N - 1 - i] = map[i][j];
+            }
+        }
+        swapDimensions(newMap);
+    }
 
-		StringBuilder sb = new StringBuilder();
-		int r = map.length;
-		int c = map[0].length;
-		for (int i = 0; i < r; i++) {
-			for (int j = 0; j < c; j++) {
-				sb.append(map[i][j] + " ");
-			}
-			sb.append("\n");
-		}
-		System.out.print(sb);
-	}
+    static void rotateLeft() {
+        int[][] newMap = new int[M][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                newMap[M - 1 - j][i] = map[i][j];
+            }
+        }
+        swapDimensions(newMap);
+    }
+
+    static void moveClockwise() {
+        int halfN = N / 2, halfM = M / 2;
+        int[][] newMap = new int[N][M];
+
+        // 1 → 2
+        copyBlock(newMap, 0, halfM, 0, 0, halfN, halfM);
+        // 2 → 3
+        copyBlock(newMap, halfN, halfM, 0, halfM, halfN, halfM);
+        // 3 → 4
+        copyBlock(newMap, halfN, 0, halfN, halfM, halfN, halfM);
+        // 4 → 1
+        copyBlock(newMap, 0, 0, halfN, 0, halfN, halfM);
+
+        map = newMap;
+    }
+
+    static void moveCounterClockwise() {
+        int halfN = N / 2, halfM = M / 2;
+        int[][] newMap = new int[N][M];
+
+        // 1 → 4
+        copyBlock(newMap, halfN, 0, 0, 0, halfN, halfM);
+        // 4 → 3
+        copyBlock(newMap, halfN, halfM, halfN, 0, halfN, halfM);
+        // 3 → 2
+        copyBlock(newMap, 0, halfM, halfN, halfM, halfN, halfM);
+        // 2 → 1
+        copyBlock(newMap, 0, 0, 0, halfM, halfN, halfM);
+
+        map = newMap;
+    }
+
+    static void copyBlock(int[][] newMap, int toRow, int toCol, int fromRow, int fromCol, int rowSize, int colSize) {
+        for (int i = 0; i < rowSize; i++) {
+            for (int j = 0; j < colSize; j++) {
+                newMap[toRow + i][toCol + j] = map[fromRow + i][fromCol + j];
+            }
+        }
+    }
+
+    static void swapDimensions(int[][] newMap) {
+        int temp = N;
+        N = M;
+        M = temp;
+        map = newMap;
+    }
+
+    static void printMap() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                sb.append(map[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
 }
