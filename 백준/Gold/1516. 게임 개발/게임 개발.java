cacc0b83841c16ff2df296a60e3cdda1597f2503
gyeongmann <hkc8857@naver.com>
2025-07-19 21:58:56 +0900

    [Gold III] Title: 게임 개발, Time: 188 ms, Memory: 19672 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.java
+++ 백준/Gold/1516. 게임 개발/게임 개발.java
@@ -6,47 +6,45 @@ public class Main {
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    int N = Integer.parseInt(br.readLine());
 	    
-	    int[] inDegree = new int[N+1];
-	    int[] dist = new int[N+1];
-	    List<int[]>[] graph = new List[N+1]; // {to, cost}
+	    int[] degree = new int[N+1];
+	    int[] cost = new int[N+1];
+	    int[] dp = new int[N+1];
+	    List<Integer>[] nextIdx = new List[N+1];
 	    for (int i = 1; i <= N; i++) {
-	        graph[i] = new ArrayList<>();
+	        nextIdx[i] = new ArrayList<>();
 	    }
 	    
 	    for (int i = 1; i <= N; i++) {
 	        StringTokenizer st = new StringTokenizer(br.readLine());
-	        int d = Integer.parseInt(st.nextToken());
-	        dist[i] = d;
+	        int c = Integer.parseInt(st.nextToken());
+	        dp[i] = cost[i] = c;
 	        
 	        while (true) {
 	            int from = Integer.parseInt(st.nextToken());
 	            if (from == -1) break;
 	            
-	            inDegree[i]++;
-	            graph[from].add(new int[] {i, d});
+	            degree[i]++;
+	            nextIdx[from].add(i);
 	        }
 	    }
 	    
 	    Queue<Integer> q = new LinkedList<>();
 	    for (int i = 1; i <= N; i++) {
-	        if (inDegree[i] == 0) q.offer(i);
+	        if (degree[i] == 0) q.offer(i);
 	    }
 	    
 	    while (!q.isEmpty()) {
 	        int curr = q.poll();
-	        for (int[] edge : graph[curr]) {
-	            int next = edge[0];
-	            int cost = edge[1];
-	            
-	            dist[next] = Math.max(dist[next], dist[curr] + cost);
-	            inDegree[next]--;
-	            if (inDegree[next] == 0) q.offer(next);
+	        for (int next : nextIdx[curr]) {
+	            dp[next] = Math.max(dp[next], dp[curr] + cost[next]);
+	            degree[next]--;
+	            if (degree[next] == 0) q.offer(next);
 	        }
 	    }
 	    
 	    StringBuilder sb = new StringBuilder();
 	    for (int i = 1; i <= N; i++) {
-	        sb.append(dist[i]).append('\n');
+	        sb.append(dp[i]).append('\n');
 	    }
 	    
 	    System.out.println(sb.toString());
