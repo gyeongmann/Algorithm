c9582c9e4bf36d09e866978cdd1750d594cc0627
gyeongmann <hkc8857@naver.com>
2025-08-06 20:26:15 +0900

    [Gold V] Title: 태상이의 훈련소 생활, Time: 2284 ms, Memory: 83548 KB -BaekjoonHub

---
+++ 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st;
+	    
+	    st = new StringTokenizer(br.readLine());
+	    int N = Integer.parseInt(st.nextToken());
+	    int M = Integer.parseInt(st.nextToken());
+	    
+	    st = new StringTokenizer(br.readLine());
+	    int[] before = new int[N];
+	    for (int i = 0; i < N; i++) {
+	        before[i] = Integer.parseInt(st.nextToken());
+	    }
+	    
+	    int[] diff = new int[N];
+	    for (int i = 0; i < M; i++) {
+	        st = new StringTokenizer(br.readLine());
+	        int a = Integer.parseInt(st.nextToken())-1;
+	        int b = Integer.parseInt(st.nextToken());
+	        int H = Integer.parseInt(st.nextToken());
+	        
+	        for (int j = a; j < b; j++) {
+	            diff[j] += H;
+	        }
+	    }
+	    
+	    StringBuilder sb = new StringBuilder();
+	    for (int i = 0; i < N; i++) {
+	        before[i] += diff[i];
+	        sb.append(before[i] + " ");
+	    }
+	    
+	    System.out.print(sb.toString());
+	}
+}
