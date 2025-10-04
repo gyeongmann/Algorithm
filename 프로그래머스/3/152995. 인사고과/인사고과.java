be0d345c5d275ae6c910a69acde2eb2c581dbd76
gyeongmann <hkc8857@naver.com>
2025-10-04 11:27:27 +0900

    [level 3] Title: 인사고과, Time: 85.00 ms, Memory: 114 MB -BaekjoonHub

---
+++ 프로그래머스/3/152995. 인사고과/인사고과.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] scores) {
+        int answer = 0;
+        int wanhoA = scores[0][0];
+        int wanhoB = scores[0][1];
+        int wanhoSum = wanhoA + wanhoB;
+        
+        Arrays.sort(scores, (a, b) -> {
+            if (a[0] == b[0]) return Integer.compare(a[1], b[1]);
+            return Integer.compare(b[0], a[0]);
+        });
+        
+        int maxB = 0;
+        int rank = 1;
+        for (int[] s : scores) {
+            int a = s[0], b = s[1];
+            
+            if (b < maxB) {
+                if (a == wanhoA && b == wanhoB) return -1;
+                continue;
+            }
+            
+            if (a + b > wanhoSum) rank++;
+            
+            maxB = Math.max(maxB, b);
+        }
+        answer = rank;
+        return answer;
+    }
+}
