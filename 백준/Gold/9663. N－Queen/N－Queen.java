94291199cabdcb0be14c5aec0f6dc9f35fb9664f
gyeongmann <hkc8857@naver.com>
2026-02-11 00:16:54 +0900

    [Gold IV] Title: N-Queen, Time: 6100 ms, Memory: 11992 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,60 +1,47 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	static int N, ans;
-	static boolean[][] visited;
 
-	static int[] dr = { -1, -1, -1 };
-	static int[] dc = { -1, 0, 1 };
+	static int N, answer;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		N = sc.nextInt(); // 보드의 행과 열 크기
-		ans = 0;
-		visited = new boolean[N][N];
-		put(0);
+		N = Integer.parseInt(br.readLine());
 
-		System.out.println(ans);
+		backTracking(new int[N], 0);
+		System.out.println(answer);
 	}
 
-	// out of bounds check
-	static boolean isValid(int nr, int nc) {
-		if (nr < 0 || nc < 0 || nr >= N || nc >= N) {
-			return false;
-		}
-
-		return true;
-	}
-
-	static boolean check(int r, int c) {
-		for (int i = 0; i < 3; i++) {
-			int nr = r + dr[i];
-			int nc = c + dc[i];
-			while (isValid(nr, nc)) {
-				if (visited[nr][nc])
-					return false;
-
-				nr += dr[i];
-				nc += dc[i];
-			}
-		}
-		return true;
-	}
-
-	// idx : 퀸을 놓을 행 위치
-	static void put(int idx) {
-		if (idx == N) {
-			ans++;
-			return;
+	static boolean backTracking(int[] track, int depth) {
+		if (depth == N) {
+			answer++;
+			return true;
 		}
 
 		for (int i = 0; i < N; i++) {
-			visited[idx][i] = true;
-			if (check(idx, i))
-				put(idx + 1);
-			visited[idx][i] = false;
+			if (!isAvailable(track, depth, i)) {
+				continue;
+			}
+
+			track[depth] = i;
+			if (!backTracking(track, depth + 1)) {
+				continue;
+			}
 		}
+
+		return false;
 	}
 
+	static boolean isAvailable(int[] track, int depth, int col) {
+		for (int row = 0; row < depth; row++) {
+			if (track[row] == col || depth - row == Math.abs(track[row] - col)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
 }
