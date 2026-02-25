2091ec74a9eabd806ea353000bbcd269310e7588
gyeongmann <hkc8857@naver.com>
2026-02-25 10:28:09 +0900

    [Gold V] Title: 기타 레슨, Time: 236 ms, Memory: 22552 KB -BaekjoonHub

---
+++ 백준/Gold/2343. 기타 레슨/기타 레슨.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N, M;
+	static int[] A;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		A = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+		}
+
+		int result = parametricSearch();
+		System.out.println(result);
+	}
+
+	private static int parametricSearch() {
+		int left = 1;
+		int right = 1_000_000_001;
+		int answer = 1_000_000_001;
+
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+
+			if (isAvailable(mid)) {
+				answer = Math.min(mid, answer);
+				right = mid - 1;
+			} else {
+				left = mid + 1;
+			}
+		}
+
+		return answer;
+	}
+
+	private static boolean isAvailable(int mid) {
+		int idx = 0;
+		int cnt = 1;
+		int sum = 0;
+		while (idx < N) {
+			if (cnt > M) {
+				return false;
+			}
+			if (sum + A[idx] > mid) {
+				sum = 0;
+				cnt++;
+				continue;
+			}
+			sum += A[idx++];
+		}
+
+		return true;
+	}
+}
